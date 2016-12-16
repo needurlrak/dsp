829501a4d008a9b099cf651713c76da5fdf5d8cc
needurlrak <karlrudeen@gmail.com>
2016-12-16 00:28:47 -0800

    Update q6_strings.py

--- python/q6_strings.py
+++ python/q6_strings.py
@@ -97,6 +97,9 @@ def mix_up(a, b):
     >>> mix_up('pezzy', 'firm')
     'fizzy perm'
     """
+    a_swapped = b[:2] + a[2:]
+    b_swapped = a[:2] + b[2:]
+    return (a_swapped + ' ' + b_swapped)
     raise NotImplementedError
 
 
