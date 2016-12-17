c577d7d294dd3fd8073577adb4e48987c709b16f
needurlrak <karlrudeen@gmail.com>
2016-12-16 20:53:32 -0800

    Update advanced_python_csv.py
    added code

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,1 +1,7 @@
-PLACE YOUR CODE HERE
+import pandas
+
+def create_email_csv():
+    facultyfile = open('faculty.csv')
+    df = pandas.read_csv(facultyfile)
+    email_list = df[' email']
+    email_list.to_csv('emails.csv')
