1fb0067f229cdd0ec4d8600ee2c5783f6e0869d6
needurlrak <karlrudeen@gmail.com>
2016-12-18 21:12:33 -0800

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -1,13 +1,28 @@
 # Hint:  use Google to find python function
 
 ####a) 
-date_start = '01-02-2013'  
-date_stop = '07-28-2015'   
+import datetime
+
+start_date = datetime.date(2013,2,1)
+stop_date = datetime.date(2015,7,28)
+
+diff = stop_date-start_date
+
+print(diff.days)
+ 
 
 ####b)  
-date_start = '12312013'  
-date_stop = '05282015'  
+start_date = datetime.date(2013,12,31)
+stop_date = datetime.date(2015,5,28)
+
+diff = stop_date-start_date
+
+print(diff.days)
 
 ####c)  
-date_start = '15-Jan-1994'  
-date_stop = '14-Jul-2015'  
+start_date = datetime.date(1994,1,15)
+stop_date = datetime.date(2015,7,14)
+
+diff = stop_date-start_date
+
+print(diff.days)
