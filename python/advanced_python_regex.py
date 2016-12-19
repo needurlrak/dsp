ff3200f17d7293792379bd70f1d2ce38815814c1
needurlrak <karlrudeen@gmail.com>
2016-12-18 21:26:59 -0800

    Update advanced_python_regex.py

--- python/advanced_python_regex.py
+++ python/advanced_python_regex.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 16:48:05 2016
+
+@author: karlrudeen
+"""
 import pandas
 
 def unique_degrees():
@@ -5,13 +12,21 @@ def unique_degrees():
     df = pandas.read_csv(facultyfile)
     faculty_degrees = list(df[' degree'])
     fixed_faculty_degrees = list()
+    deg_dict = dict()
     for deg in faculty_degrees:
         if (deg != '0'):
             temp_deg = deg.upper()
             temp_deg = temp_deg.replace('.', '')
             temp_degs = temp_deg.split()
-            fixed_faculty_degrees = fixed_faculty_degrees + temp_degs
-    return(len(set(fixed_faculty_degrees)))
+            #the below loop is necessary for faculty members with multiple degrees.
+            for degree in temp_degs:
+                if (degree in deg_dict):
+                    deg_dict[degree] += 1
+                else:
+                    deg_dict[degree] = 1
+            fixed_faculty_degrees = fixed_faculty_degrees + temp_degs #this line is probably pretty inefficient.
+    print(len(set(fixed_faculty_degrees)))
+    return(deg_dict)
       
 def unique_titles():
     facultyfile = open('faculty.csv')
@@ -40,4 +55,9 @@ def unique_domains():
         domain = address[address.index('@'):]
         domain_list.append(domain)
     return(set(domain_list))
+    
+    
+    
+    
+    
     
